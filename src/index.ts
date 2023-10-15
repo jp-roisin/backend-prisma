c4f26f6c1c9e8c6ebaa1dc71424c1b131a6e5c25
jp-roisin <jeanpaul.roisin@protonmail.com>
2023-10-15 11:32:07 +0200

    Implement JWT check middleware

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 import userRouter from "./routes/user";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Middleware function to authenticate JWT
+const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.header("Authorization");
+
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET ?? "");
+    // req.user = decoded;
+    console.log(decoded);
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Token is invalid" });
+  }
+};
 
 const app = express();
 
@@ -9,6 +31,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes
-app.use("/user", userRouter);
+app.use("/user", userRouter, authenticateJWT);
 
 app.listen(port);
